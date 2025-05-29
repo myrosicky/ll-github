d0193e7ea20695de3480b965672247bd7282688c
myrosicky <myrosicky@163.com>
2025-05-29 22:40:39 +0800

    init

--- src/main/java/org/ll/git/svc/PullRequestServiceImpl.java
+++ src/main/java/org/ll/git/svc/PullRequestServiceImpl.java
@@ -1,24 +1,28 @@
 package org.ll.git.svc;
 
+import io.netty.handler.ssl.ApplicationProtocolConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.time.Duration;
+
 
 @Service
 @Slf4j
 public class PullRequestServiceImpl {
 
     @Value("${github.api.url:https://api.github.com}") private String githubApiUrl;
-    @Value("${github.api.username}") private String username;
-    @Value("${github.api.token}") private String token;
 
-    public void createPullRequest(String repo, String baseBranch, String fromBranch,
-                                        String title, String body) {
+    public Mono<String> createPullRequest(String repoOwnerName, String repoName, String token, String baseBranch, String fromBranch,
+                                    String title, String body) {
 
         String jsonBody = """
         {
@@ -28,42 +32,73 @@ public class PullRequestServiceImpl {
             "base": "%s"
         }
         """.formatted(title, body, fromBranch, baseBranch);
+        reactor.netty.http.client.HttpClient httpClient = HttpClient.create()
+                    .protocol(HttpProtocol.H2)
+                    .responseTimeout(Duration.ofSeconds(5));
 
-
-        HttpClient client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
+        WebClient webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .baseUrl(githubApiUrl)
                 .build();
 
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("%s/repos/%s/%s/pulls".formatted(githubApiUrl, username, repo)))
+        return webClient.post()
+                .uri("%s/repos/%s/%s/pulls".formatted(githubApiUrl, repoOwnerName, repoName))
                 .header("Accept", "application/vnd.github+json")
-                .header("Authorization", "Bearer %s".formatted(token))
+                .header("Authorization", "token %s".formatted(token))
                 .header("X-GitHub-Api-Version", "2022-11-28") // refer: https://docs.github.com/en/rest/about-the-rest-api/api-versions
-                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
-                .build();
-
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(response -> {
-                    return switch (response.statusCode()) {
-                        case 201 -> {
-                            log.info("Pull Request success");
-                            yield response.body();
-                        }
-                        case 401 -> {
-                            log.info("401");
-                            yield null;
-                        }
-                        case 404 -> {
-                            log.info("repository not found");
-                            yield null;
-                        }
-                        default -> {
-                            log.info("failed - {}, {}", response.statusCode(), response.body());
-                            yield null;
-                        }
-                    };
+                .bodyValue(jsonBody)
+                .retrieve()
+                .onRawStatus(s -> s == 401, resp -> Mono.error(new Exception("401")))
+                .onRawStatus(s -> s == 404, resp -> {
+                    log.info("repository not found");
+                    return Mono.error(new Exception("repository not found"));
                 })
-                .join();
+                .onStatus(HttpStatusCode::is5xxServerError, resp -> {
+                    log.warn("failed with: {}", resp.statusCode().value());
+                    return Mono.error(new Exception("failed"));
+                })
+                .bodyToMono(String.class)
+                .doOnError(t -> log.error("createPullRequest failed", t))
+                .doOnNext(respBody -> {
+                    log.info("Pull Request success");
+                })
+                ;
+
+
+//        HttpClient client = HttpClient.newBuilder()
+//                .version(HttpClient.Version.HTTP_2)
+//                .build();
+//
+//
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("%s/repos/%s/%s/pulls".formatted(githubApiUrl, repoOwnerName, repoName)))
+//                .header("Accept", "application/vnd.github+json")
+//                .header("Authorization", "token %s".formatted(token))
+//                .header("X-GitHub-Api-Version", "2022-11-28") // refer: https://docs.github.com/en/rest/about-the-rest-api/api-versions
+//                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+//                .build();
+//
+//        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(response -> {
+//                    return switch (response.statusCode()) {
+//                        case 201 -> {
+//                            log.info("Pull Request success");
+//                            yield response.body();
+//                        }
+//                        case 401 -> {
+//                            log.info("401");
+//                            yield null;
+//                        }
+//                        case 404 -> {
+//                            log.info("repository not found");
+//                            yield null;
+//                        }
+//                        default -> {
+//                            log.info("failed - {}, {}", response.statusCode(), response.body());
+//                            yield null;
+//                        }
+//                    };
+//                })
+//                .join();
     }
 }
--- src/test/java/org/ll/git/svc/PullRequestServiceImplTest.java
+++ src/test/java/org/ll/git/svc/PullRequestServiceImplTest.java
@@ -3,7 +3,9 @@ package org.ll.git.svc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,10 +14,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class PullRequestServiceImplTest {
 
     @Autowired private PullRequestServiceImpl svc;
+    @Value("${github.api.token}") private String token;
 
     @Test
-    void a(){
-        svc.createPullRequest("https://github.com/myrosicky/ll-github.git", "master", "pos/a",
-                "test pr title", "test pr message haha");
+    void createPullRequest(){
+        StepVerifier.create(svc.createPullRequest("myrosicky", "ll-github", token,
+                "master", "poc/a",
+                "test pr title", "test pr message haha"))
+                .expectComplete()
+        ;
     }
 }
